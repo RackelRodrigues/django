e3b61117493f33a3e2077cb504cab285779250ad
RackelRodrigues <rackelrodrigues247@gmail.com>
2023-05-02 16:57:01 -0300

    more one day

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,14 +1,8 @@
-from recipes.views import home
 from django.urls import path
 
+from . import views
 
-
-
-
-
-
-
-
-urlpatterns = {
-   path('', home)
-}
+urlpatterns = [
+   path('', views.home),
+   path('recipes/<id>/', views.recipe),
+]
--- recipes/views.py
+++ recipes/views.py
@@ -3,6 +3,17 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+        return render(request, 'recipes/pages/home.html', context={
+                
+       'name': 'rackel',
+})
+
+
+
+def recipe(request, id):
+        return render(request, 'recipes/pages/home.html', context={
+                
+       'name': 'rackel',
+    })
 
 
