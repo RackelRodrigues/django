fa8c1fa43755c6928d650b9e567dbf0ad46061ea
RackelRodrigues <rackelrodrigues247@gmail.com>
2023-05-06 16:47:05 -0300

    melhorias

--- recipes/views.py
+++ recipes/views.py
@@ -1,19 +1,19 @@
 from django.shortcuts import render
 
+from utils.recipes.factory import make_recipe
+
 
 # Create your views here.
 def home(request):
         return render(request, 'recipes/pages/home.html', context={
-                
-       'name': 'rackel',
+        'recipes': [make_recipe() for _ in rangue(10)],
 })
 
 
 
 def recipe(request, id):
         return render(request, 'recipes/pages/home.html', context={
-                
-       'name': 'rackel',
+        'recipe': make_recipe(),
     })
 
 
