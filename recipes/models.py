31b55eca16a0be13b088554651d796eb65fbf5eb
RackelRodrigues <rackelrodrigues247@gmail.com>
2023-05-13 17:51:35 -0300

    .

--- recipes/models.py
+++ recipes/models.py
@@ -1,7 +1,29 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
 
+class Category(models.Model):
+   name = models.CharField(max_length=65)
+
+
 
 class Recipe(models.Model):
-ssssss
+    title = models.CharField(max_length=65)
+    description = models.CharField(max_length=165)
+    slug = models.SlugField()
+    preparation_time = models.IntegerFiled()
+    preparation_time_unit= models.CharField(max_length=65)
+    servings_unit = models.CharField(max_length=65)
+    preparation_steps= models.TextField()
+    preparetion_steps_is_html = models. BooleanField(default=False)
+    created_at = models.DaTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    is_publish = models. BooleanField(default=False)
+    cover = models.ImageField(upload_to='recepes/covers/%Y/%m/%d/')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True 
+)
+    author = models.ForeignKey(
+        Category, on_delete=models.SET_NULL, null=True 
+)
--- recipes/views.py
+++ recipes/views.py
@@ -6,16 +6,15 @@ from utils.recipes.factory import make_recipe
 # Create your views here.
 def home(request):
     return render(request, 'recipes/pages/home.html', context={
-          'recipes': [make_recipe() for _ in range(10)],
+       'recipes': [make_recipe() for _ in range(10)],
 });
 
 
 
 def recipe(request, id):
   return render(request, 'recipes/pages/home.html', context={
-   'recipe': make_recipe(),
-    'is_detail_page': True,
-          
+     'recipe': make_recipe(),
+     'is_detail_page': True,
 });
 
 
